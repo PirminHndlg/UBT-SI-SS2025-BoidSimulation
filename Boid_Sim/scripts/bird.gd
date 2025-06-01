a3bd8edac7022d986a8536c9e9fed2250cf8e49a
Pirmin <pirmin@absinth.net>
2025-06-01 14:21:04 +0200

    updated files, loaded wrong zip know it is right

--- Boid_Sim/scripts/bird.gd
+++ Boid_Sim/scripts/bird.gd
@@ -2,15 +2,19 @@
 # CC0 1.0 Universal License (CC0 1.0) Public Domain Dedication (https://creativecommons.org/publicdomain/zero/1.0/)
 
 extends Node3D
-
+@export var max_initial_speed: float = 0.00005
+@export var velocity: Vector3 = Vector3.ZERO
 func _ready() -> void:
 	# Set up Animation
 	var flying_anim = $Pigeon/AnimationPlayer2.get_animation("flying_anims/flying")
 	flying_anim.loop = true
 	$Pigeon/AnimationPlayer2.play("flying_anims/flying")
-	
+	var speed = randf_range(0.0, max_initial_speed)
+	velocity*=speed
+	look_at(position+velocity)
 	return
 	
 
 func _physics_process(_delta: float) -> void:
+	position+=velocity
 	pass
--- Boid_Sim/scripts/main.gd
+++ Boid_Sim/scripts/main.gd
@@ -2,9 +2,29 @@
 # CC0 1.0 Universal License (CC0 1.0) Public Domain Dedication (https://creativecommons.org/publicdomain/zero/1.0/)
 
 extends Node3D
+var y_range = Vector2(10,20)
+var x_range = Vector2(-20,5)
+var z_range = Vector2(-5,20)
 
+@onready var birds_root: Node3D = $Birds
+
+const BIRD = preload("res://scenes/bird.tscn")
 func _ready() -> void:
+	for i in range(randi_range(30,60)):
+		var instance = BIRD.instantiate()
+		instance.position =Vector3(randf_rangeV(x_range),randf_rangeV(z_range),randf_rangeV(y_range))
+		birds_root.add_child(instance)
+		var random_direction = Vector3(
+			randf_range(-1.0, 1.0),
+			randf_range(-1.0, 1.0),
+			randf_range(-1.0, 1.0)
+		).normalized()
+		instance.velocity = random_direction
+		print_debug("Spawned"+str(i)+" birdoid")
 	pass
 	
 func _physics_process(_delta: float) -> void:
 	pass
+	
+func randf_rangeV(range: Vector2)->float:
+	return randf_range(range[0],range[1])
